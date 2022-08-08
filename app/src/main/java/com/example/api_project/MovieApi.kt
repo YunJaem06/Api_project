309ac2de3572c5db43fcdf540c0e61c68998c8f8
YunJaem06 <yunjae0616@gmail.com>
2022-08-08 19:14:48 +0900

    CGV 어플 splash 화면 구현완료

---
+++ app/src/main/java/com/example/api_project/MovieApi.kt
@@ -0,0 +1,8 @@
+package com.example.api_project
+
+data class MovieApi(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
---
+++ app/src/main/java/com/example/api_project/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.api_project
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.api_project.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ObjectAnimator.ofFloat(binding.ivSplash, View.TRANSLATION_X, -700f).apply {
+            duration = 1500L
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+            start()
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
+}
